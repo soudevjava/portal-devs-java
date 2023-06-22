061edc60d7206c333ff2866eed969f96e3bd9192
soudevjava <soudevjava@gmail.com>
2023-06-22 14:33:37 -0300

    projeto criado

---
+++ src/test/java/com/projetodevsjava/portal/PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetodevsjava.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
