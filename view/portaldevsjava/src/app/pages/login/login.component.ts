03105da43e776fb0a3864a7b1de11b9ce1730dff
Lucas Sousa <lucas.sousa_99@hotmail.com>
2023-06-23 15:26:48 -0300

    [Add] Componentes tela login

--- view/portaldevsjava/src/app/pages/login/login.component.ts
+++ view/portaldevsjava/src/app/pages/login/login.component.ts
@@ -1,40 +1,39 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { IUsuario } from './IUsuario';
 import { UsuarioService } from '../../services/usuario.service';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  formLogin: FormGroup;
+  formLogin!: FormGroup;
+  constructor(private formBuilder: FormBuilder,
+              private usuarioService: UsuarioService,
+              private snackBar: MatSnackBar) { }
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private usuarioService: UsuarioService,
-    private snackBar: MatSnackBar
-  ) {
+
+
+  ngOnInit(): void {
+    this.criarForm();
+  }
+  criarForm(){
     this.formLogin = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      senha: ['', [Validators.required]]
     });
   }
-
-  ngOnInit(): void {}
-
-  logar() {
-    if (this.formLogin.invalid) return;
-
-    const usuario = this.formLogin.getRawValue() as IUsuario;
+  logar(){
+    if(this.formLogin.invalid) return;
+    var usuario = this.formLogin.getRawValue() as IUsuario;
     this.usuarioService.logar(usuario).subscribe((response) => {
-      if (!response.sucesso) {
-        this.snackBar.open('Falha na autenticação', 'Usuário ou senha incorretos.', {
-          duration: 3000
-        });
-      }
-    });
+        if(!response.sucesso){
+          this.snackBar.open('Falha na autenticação', 'Usuário ou senha incorretos.', {
+            duration: 3000
+          });
+        }
+    })
   }
 }
